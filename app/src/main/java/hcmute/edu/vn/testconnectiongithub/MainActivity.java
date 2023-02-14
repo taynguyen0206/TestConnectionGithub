74edacf3f3a7b8fc1dcff78d7a233d68e47ef615
taynguyen0206 <phamtrinhtaynguyen2002@gmail.com>
2023-02-14 23:29:11 +0700

    Initial commit

---
+++ app/src/main/java/hcmute/edu/vn/testconnectiongithub/MainActivity.java
@@ -0,0 +1,14 @@
+package hcmute.edu.vn.testconnectiongithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
